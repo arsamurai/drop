b1531e83ac8e47e9038f69a0890f0d481de5425c
Artur Musienko <musienkoartur45@gmail.com>
2024-08-26 12:43:59 +0300

    fix move-net

--- src/ts/move-net.ts
+++ src/ts/move-net.ts
@@ -2,13 +2,10 @@ const banner = document.getElementById("profitsale-baner") as HTMLElement
 const net = document.getElementById("animated-net") as HTMLElement
 
 banner?.addEventListener("mousemove", e => {
-  const rect = banner.getBoundingClientRect()
+	const rect = banner.getBoundingClientRect()
   const mouseX = e.clientX - rect.left
-
   const centerX = rect.width / 2
-
   const deltaX = (mouseX - centerX) / centerX
-
   const translateX = -5 * deltaX
 
   net.style.transform = `translateX(${translateX}%)`
