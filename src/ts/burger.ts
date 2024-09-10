4d29040170acd87dbe09bf4a262d388817a10491
Artur Musienko <musienkoartur45@gmail.com>
2024-09-10 11:33:29 +0300

    add ru lang

--- src/ts/burger.ts
+++ src/ts/burger.ts
@@ -7,7 +7,7 @@ const setModalState = (btn: HTMLElement, menu: HTMLElement, activate: boolean): 
 
 const burger = document.getElementById("burger") as HTMLButtonElement
 const menu = document.getElementById("menu") as HTMLElement
-const menuLinks = menu?.querySelectorAll("[data-menu-link]")
+const menuLinks = menu?.querySelectorAll("[data-menu-link]") as NodeListOf<HTMLLIElement>
 
 burger?.addEventListener("click", ({ currentTarget }) => {
   const target = currentTarget as HTMLElement
@@ -17,7 +17,13 @@ burger?.addEventListener("click", ({ currentTarget }) => {
 })
 
 menuLinks?.forEach(link => {
+  const linkTagA = link?.querySelector("a")
+
   link.addEventListener("click", () => {
     setModalState(burger, menu, false)
   })
+
+  if (linkTagA?.href === window.location.href) {
+    linkTagA.classList.add("text-primary")
+  }
 })
